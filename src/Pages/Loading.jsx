663745c22b1e7c67e650bfcf82d56231570f17e5
Wissem Sahli <contact.ing.wissem@gmail.com>
2025-11-22 19:18:40 +0100

    XD

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -49,7 +49,7 @@ export default function LoadingPage() {
           top: 0,
           left: 0,
           width: '100%',
-          height: '100%',
+          height: '100vh',
           backgroundColor: '#000',
           display: 'flex',
           flexDirection: 'column',
